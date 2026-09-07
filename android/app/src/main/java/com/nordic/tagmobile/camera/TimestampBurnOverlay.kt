d0ef6825c2571d988e47aa903b20cb636307a6cf
Gokul <sg.gokul26@gmail.com>
2026-09-07 15:43:37 +0530

    Fix EGL constant refs in TimestampBurnOverlay for CI compile.

--- android/app/src/main/java/com/nordic/tagmobile/camera/TimestampBurnOverlay.kt
+++ android/app/src/main/java/com/nordic/tagmobile/camera/TimestampBurnOverlay.kt
@@ -116,8 +116,8 @@ class TimestampBurnOverlay(
             EGL14.EGL_GREEN_SIZE, 8,
             EGL14.EGL_BLUE_SIZE, 8,
             EGL14.EGL_ALPHA_SIZE, 8,
-            EGL14.EGL_RENDERABLE_TYPE, EGLExt.EGL_OPENGL_ES2_BIT,
-            EGL14.EGL_RECORDABLE_ANDROID, 1,
+            EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+            EGLExt.EGL_RECORDABLE_ANDROID, 1,
             EGL14.EGL_NONE,
         )
         val configs = arrayOfNulls<EGLConfig>(1)
